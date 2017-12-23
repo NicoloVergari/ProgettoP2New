ed4ad03fe01a54775f42a02be7862160ac8a5ef5
Matteo <matt.slanzi95@gmail.com>
2017-12-23 14:37:15 +0100

    Add files via upload

---
+++ listachar.cpp
@@ -0,0 +1,37 @@
+#include"listachar.h"
+#include<iostream>
+using namespace std;
+
+ListaChar::ListaChar(char i, ListaChar * n) : info(i), next(n) {}
+
+ListaChar * ListaChar::costruisci(int dim){
+    if(dim){
+        char x;
+        cin>>x;
+        return new ListaChar(x,costruisci(dim-1));
+    }
+    else
+        return 0;
+}
+
+void ListaChar::visualNodo(ListaChar *l) const {
+    cout<<l->info<<" -- ";
+    cout<<l;
+}
+
+void ListaChar::visualNodoNext(ListaChar *l) const{
+    cout<<l->next->info<<" -- ";
+    cout<<l->next;
+}
+
+ListaChar * ListaChar::aggiungiNodo(char x, ListaChar *l){
+    ListaChar *a=new ListaChar(x,l);
+    return a;
+}
+
+int ListaChar::trova(char *a, ListaChar *l) const {
+    if(!a)
+        return 2;
+    if(a)
+        return trova(a+1, l->next);
+}
