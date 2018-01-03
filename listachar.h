68387ddc9f6f3c7e527d98eec4416857034edc1c
Matteo <matt.slanzi95@gmail.com>
2018-01-03 18:36:17 +0100

    Update listachar.h

--- listachar.h
+++ listachar.h
@@ -1,27 +1,28 @@
-#ifndef LISTACHAR_H
-#define LISTACHAR_H
-
 class ListaChar {
  private:
     char info;
     ListaChar *next;
+
+    static bool equals(ListaChar*, ListaChar*);
+
  public:
+    ListaChar();
     ListaChar(char, ListaChar *);
-    ListaChar(const ListaChar &);
+    //ListaChar(const ListaChar &);
     ListaChar& operator=(const ListaChar &);
-    ~ListaChar();
+    //~ListaChar();
     ListaChar *costruisci(int);
-    ListaChar *togliNodo();
-    ListaChar *togliNodo(char);
+    ListaChar *togliNodo(ListaChar *, char);
     ListaChar *togliNodo(int);
     ListaChar *ordina();
-    int trova(char *, ListaChar *) const;
+    bool trova(char *, ListaChar *);
+    bool operator==(const ListaChar &) const;
     ListaChar* operator+(ListaChar *);
-    ListaChar *unisci(ListaChar *);
-    void visualNodoChar(ListaChar *) const;
-    void visualNodo(ListaChar *) const;
-    void visualNodoNext(ListaChar *) const;
+    ListaChar *unisci(ListaChar *, ListaChar *);
     ListaChar * aggiungiNodo(char, ListaChar *);
+    void visualizzaLista(ListaChar *) const;
+    ListaChar *reverse(ListaChar *);
+    ListaChar *insRe(ListaChar *, ListaChar *);
 };
 
 #endif // LISTACHAR_H
