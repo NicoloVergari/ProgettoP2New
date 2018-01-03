0496be17a5d1ad76756319cc9737973a8ba4ac3d
Matteo <matt.slanzi95@gmail.com>
2018-01-03 18:35:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,21 +2,55 @@
 #include<iostream>
 using namespace std;
 
+
+
 int main()
 {
-    ListaChar *a=new ListaChar('g',0);
-    /*a->visualNodo(a);
-    cout<<endl<<"-----------"<<endl;*/
 
-    a=a->aggiungiNodo('s',a);
-    /*a->visualNodo(a);
-    cout<<endl;
-    a->visualNodoNext(a);
-    cout<<endl;*/
-    char s[]="sg";
-    cout<<endl;
+    cout<<"inserisci dimensione lista di char: ";
+	int dim;
+	cin>>dim;
+	ListaChar *a=0;
+	a=a->costruisci(dim);
+	a->visualizzaLista(a);
 
-    int tr;
-    tr=a->ListaChar::trova(s,a);
-    cout<<tr;
+	cout<<"--------"<<endl;
+	cout<<"inserisci dimensione lista di char: ";
+	int dim2;
+	cin>>dim2;
+	ListaChar *b=0;
+	b=b->costruisci(dim2);
+	b->visualizzaLista(b);
+
+	if(a==b)
+		cout<<"le liste sono uguali";
+	else
+		cout<<"le liste non sono uguali";
+
+	/*a=a->togliNodo(a,'o');
+    cout<<"---------"<<endl;
+    a=a->reverse(a);
+	a->visualizzaLista(a);*/
+
+
+
+	/*cout<<"inserisci match da trovare nella lista: ";
+    char s[dim]={};
+    cin>>s;
+    bool m=a->trova(s,a);
+    if(m)
+    	cout<<"il match è stato trovato."<<endl;
+    else
+    	cout<<"il match non è stato trovato."<<endl;
+    cout<<"--------"<<endl<<"inserisci nuova lista, dimensione: ";
+    int dim2;
+    cin>>dim2;
+    ListaChar *b=0;
+    b=b->costruisci(dim2);
+    b->visualizzaLista(b);
+    cout<<"Lista Unita"<<endl;
+    ListaChar *p=a;
+    a=a->unisci(a,b);
+    p->visualizzaLista(p);
+    */
 }
